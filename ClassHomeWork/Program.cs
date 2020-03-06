7827cf06f47217f8562fedb39516f5b08e8570c6
BatOleh <61079990+BatOleh@users.noreply.github.com>
2020-03-06 23:02:07 +0200

    Add files via upload

---
+++ ClassHomeWork/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassHomeWork
+{
+    class Text
+    {
+        public String S="";
+        public Text()
+        {
+            //S = "Here is the fashion to be your ad :)";
+            S = Console.ReadLine();
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Text ad = new Text();
+            Console.WriteLine(ad.S);
+            Console.ReadKey();
+        }
+    }
+}
